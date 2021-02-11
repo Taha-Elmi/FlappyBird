a0ef09e3f731e0f8df7118859fb97fca5ba63b23
Taha-Elmi <elmi.taha@gmail.com>
2021-02-11 08:13:47 +0330

    walls and their movement added

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import random
 
 pygame.init()
 
-mainWindow = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('The First Game with Python')
+mainWindow = pygame.display.set_mode((500, 400))
+pygame.display.set_caption('Flappy Bird')
 
 r = 0
 g = 0
 b = 0
 color = (r, g, b)
-
 white = (255, 255, 255)
-rect1 = pygame.Rect(100, 100, 100, 100)
-clock = pygame.time.Clock()
 
+upper_wall = pygame.Rect(100, 0, 40, 120)
+lower_wall = pygame.Rect(100, 280, 40, 120)
+
+clock = pygame.time.Clock()
 dif = 5
+upper_wall_height = 120
 
 while True:
     
@@ -28,14 +30,22 @@ while True:
     mainWindow.fill(white)
 
     color = (r, g, b)
-    pygame.draw.rect(mainWindow, color, rect1)
+    pygame.draw.rect(mainWindow, color, upper_wall)
+    pygame.draw.rect(mainWindow, color, lower_wall)
 
-    rect1.x += dif
-    if rect1.x >= 300 or rect1.x <= 0:
-        dif *= -1
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
+    upper_wall.x -= dif
+    lower_wall.x -= dif
+    if upper_wall.x <= -40:
+        upper_wall.x = 400
+        lower_wall.x = 400
+
+        upper_wall_height = random.randint(40, 200)
+        upper_wall.h = upper_wall_height
+        lower_wall.h = 240 - upper_wall_height
+        lower_wall.y = 400 - lower_wall.h
+        #r = random.randint(0, 255)
+        #g = random.randint(0, 255)
+        #b = random.randint(0, 255)
 
 
     clock.tick(30)
