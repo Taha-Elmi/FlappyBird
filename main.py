0b54d45847345424114aa412baf3462e2a6f47e0
TahaElmi <elmi.taha@gmail.com>
2021-04-08 08:46:34 +0430

    feat: bird rect added

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ pygame.init()
 mainWindow = pygame.display.set_mode((500, 400))
 pygame.display.set_caption('Flappy Bird')
 
-r = 0
-g = 0
-b = 0
-color = (r, g, b)
+black = (0, 0, 0)
 white = (255, 255, 255)
 
 upper_wall = pygame.Rect(100, 0, 40, 120)
 lower_wall = pygame.Rect(100, 280, 40, 120)
+bird = pygame.Rect(130, 50, 50, 50)
 
 clock = pygame.time.Clock()
 dif = 5
+v_bird = 0
+a_bird = 1
 upper_wall_height = 120
 
 while True:
@@ -29,23 +29,28 @@ while True:
             
     mainWindow.fill(white)
 
-    color = (r, g, b)
-    pygame.draw.rect(mainWindow, color, upper_wall)
-    pygame.draw.rect(mainWindow, color, lower_wall)
+    pygame.draw.rect(mainWindow, black, upper_wall)
+    pygame.draw.rect(mainWindow, black, lower_wall)
+    pygame.draw.rect(mainWindow, black, bird)
 
     upper_wall.x -= dif
     lower_wall.x -= dif
+
+    v_bird += a_bird
+    bird.y += v_bird
+
+    keypressed = pygame.key.get_pressed()
+    if keypressed[pygame.K_SPACE]:
+        v_bird = -7
+    
     if upper_wall.x <= -40:
-        upper_wall.x = 400
-        lower_wall.x = 400
+        upper_wall.x = 500
+        lower_wall.x = 500
 
         upper_wall_height = random.randint(40, 200)
         upper_wall.h = upper_wall_height
         lower_wall.h = 240 - upper_wall_height
         lower_wall.y = 400 - lower_wall.h
-        #r = random.randint(0, 255)
-        #g = random.randint(0, 255)
-        #b = random.randint(0, 255)
 
 
     clock.tick(30)
